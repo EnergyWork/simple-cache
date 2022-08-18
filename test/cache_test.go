85117419063d1275ec90f580dacef8272286db64
Artyom <ag.energy@mail.ru>
2022-08-19 02:38:55 +0300

    fix

--- test/cache_test.go
+++ test/cache_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/energywork/simple-cache/internal"
 	cache "github.com/energywork/simple-cache/pkg"
@@ -31,4 +32,21 @@ func TestCache(t *testing.T) {
 func TestCacheNil(t *testing.T) {
 	c := cache.Cache{}
 	c.Set("a", 1, 0)
+}
+
+func TestExpiredCache(t *testing.T) {
+	c := cache.New()
+	c.Set("key", 999, 3*time.Second)
+	t.Log("added element")
+	_, err := c.Get("key")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("after Get value still exist")
+	time.Sleep(4 * time.Second)
+	_, err = c.Get("key")
+	if err != internal.ErrExpired || err != internal.ErrNotFound {
+		t.Fatal(err)
+	}
+	t.Log("value exired")
 }
